81dad7d16929f619c7c5d332c2746b73818d01cc
admin42045 <nitinkumar42045@gmail.com>
2020-08-08 15:24:46 +0530

    Initial thired commit in js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 // 01 chap // and javascript can use in three types - 1-Internal , 2-Inline  , 3-External
 
 
@@ -22,8 +23,8 @@ document.getElementsById("innerhtml").innerHTML = "InnerHTML Example";
 // commiting in javascript
 // single line commit
 
-/* multi line commit
-byt it's line */
+// multi line commit
+//byt it's line 
 
 document.getElementsByClassName("math").innerHTML = "constants in js";
 
@@ -89,3 +90,161 @@ document.getElementById("demo").innerHTML = Math.max(23,232,232,32,3343,54);
 
 // math min , max
 document.getElementById("demo").innerHTML = Math.min(23,232,232,32,3343,54);
+
+
+
+// round function 
+document.getElementById("demo").innerHTML = Math.round(15.90);
+
+
+
+// arrays
+var cars = ["audi" , "Safari" , "Bulero"]; // array
+document.getElementById("demo").innerHTML = cars[2];
+
+// arrays - push(), shift(), unshift(), pop()
+
+var fruits = ["apple", "pear", "mango" , "Oranger" , "pineappple" ,"gawava"]; //arrays
+//fruits.pop();
+//fruits.shift();
+//fruits.push("Carrot");
+//fruits.unshift();
+document.getElementById("demo").innerHTML = fruits.join();
+
+
+// javascript changing and deleting elements
+var fruits = ["apple", "pear", "mango" , "Oranger" , "pineappple" ,"gawava"]; //arrays
+//fruits.pop();
+//fruits.shift();
+//fruits.push("Carrot");
+//fruits.unshift();
+
+//fruits[0] = "kiwi";
+//delete fruits[0];
+
+document.getElementById("demo").innerHTML = fruits.join();
+
+
+
+// splice in js
+var fruits = ["apple", "pear", "mango" , "Oranger" , "pineappple" ,"gawava"]; //arrays
+//fruits.pop();
+//fruits.shift();
+//fruits.push("Carrot");
+//fruits.unshift();
+
+fruits.slice(1,0, "Lemon" , "Banana");
+document.getElementById("demo").innerHTML = fruits.join();
+
+
+
+// sort in js
+var cars = ["BMW", "Safari", "tata" ,"mahandra"];
+cars.sort();
+document.getElementById("demo").innerHTML = cars;
+
+
+
+
+// concat in js
+var cars = ["BMW", "Safari", "tata" ,"mahandra"];
+var boys = ["Boby", "Sonu", "Targen"];
+var combined = cars.concat(boys);
+document.getElementById("demo").innerHTML = combined;
+
+
+
+
+
+var mescore = 65;
+if ( mescore < 40 )
+{
+    document.write("score greater than 40");
+}
+else if (mescore < 70 ){
+    document.write(" score less than 70 ");
+}
+else if (mescore < 80 ){
+    document.write(" score less than 80 ");
+}
+
+else if (mescore < 90 ){
+    document.write(" score less than 90 ");
+}
+else {
+   document.write("score must be less than of 100");
+}
+
+
+
+
+
+// js comparision
+var x = 54;
+document.getElementById("demo").innerHTML = (x == 54);
+
+var b1 = Boolean(90);
+var b2 = Boolean(2.86);
+document.getElementById("demo").innerHTML = 
+"90 is " + b1 + "<br>"+ "2.82 is " + b2 ;
+
+
+
+
+for( var i = 0; i <= 10; i++){
+document.write(i);
+document.write(" <br />");
+
+}
+
+var i;
+for( i = 0; i< 10; i++) {
+    if( i === 3)
+    {
+        //break;
+    }
+    else {
+
+    continue;
+    }
+    document.writeln(i+ "<br>");
+}
+
+// function in js
+
+function add( a, b){
+    return (a + b);
+}
+function mult( a, b){
+    return (a * b);
+}
+
+function sub( a, b){
+    return (a - b);
+}
+function divi( a, b){
+    return (a / b);
+}
+function modu( a, b){
+    return (a % b);
+}
+document.getElementById("demo").innerHTML = add( 6 , 23);
+document.getElementById("demo").innerHTML = mult( 9 , 23);
+document.getElementById("demo").innerHTML = sub( 7 , 23);
+document.getElementById("demo").innerHTML = divi( 3 , 23);
+document.getElementById("demo").innerHTML = modu( 7 , 23);
+
+*/
+
+// js event - mouse , mouseover, mousecut, key down
+document.getElementById("demo").innerHTML = Date();
+
+
+
+
+
+
+
+
+
+
