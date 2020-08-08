91885cf2cc3c3f8f376a4988f00cd506b7d250c6
admin42045 <nitinkumar42045@gmail.com>
2020-08-08 10:56:57 +0530

    initial second commit

--- script.js
+++ script.js
@@ -9,7 +9,6 @@
 
 // 2-Inline
 
-
 // External style
 document.getElementById("one").innerHTML = "External style of JavaScript";
 
@@ -50,4 +49,43 @@ document.innerHTML(num3);
 var age = 29;
 var lastname = "Kumar";
 var cars = ["audi" , "Safari" , "Bulero"]; // array
-var person = {firstname: "Nitin" , lastname: "Kumar" , age: 23, gender:"male"}; // object
+var person = {firstname: "Nitin" , lastname: "Kumar" , age: 23, gender:"male"}; // objec:t
+
+
+
+// object
+var cars = {type: "Acura" , model: "Integra" , color: " white"};
+document.getElementById("demo").innerHTML = cars.type;
+
+
+// object output
+var cars = {type: "Acura" , model: "Integra" , color: " white"};
+document.getElementById("demo").innerHTML = cars.type + " is" + cars.color;
+
+
+// javascript strings
+var studentname1 = "Nitin Kumar";
+var studentname2 = "rohit Gautam Kumar";
+document.getElementById("demo").innerHTML = studentname1 + "<br>" + studentname2;
+
+// javascript strings length
+var studentname1 = "Nitin Kumar";
+var studentname2 = "rohit Gautam Kumar";
+document.getElementById("demo").innerHTML = studentname1.length; 
+
+
+// special character
+var spec = "We are the best'players' in the world"
+document.getElementById("demo").innerHTML = spec;
+
+
+// random number in js
+document.getElementById("demo").innerHTML = Math.random();
+
+
+
+// math min , max
+document.getElementById("demo").innerHTML = Math.max(23,232,232,32,3343,54);
+
+// math min , max
+document.getElementById("demo").innerHTML = Math.min(23,232,232,32,3343,54);
