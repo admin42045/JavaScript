8ee89e72f87fc598231e72876cf005b9c9f0d1be
admin42045 <ntinkumar42045@gmail.com>
2020-08-08 10:21:34 +0530

    Initial Js commit

---
+++ script.js
@@ -0,0 +1,53 @@
+// 01 chap // and javascript can use in three types - 1-Internal , 2-Inline  , 3-External
+
+
+
+// 1-Internal style
+
+
+
+
+// 2-Inline
+
+
+// External style
+document.getElementById("one").innerHTML = "External style of JavaScript";
+
+// output of js
+document.getElementsByClassName("output").innerHTML = "Java script output here!";
+
+// innerHTML 
+document.getElementsById("innerhtml").innerHTML = "InnerHTML Example";
+
+
+// commiting in javascript
+// single line commit
+
+/* multi line commit
+byt it's line */
+
+document.getElementsByClassName("math").innerHTML = "constants in js";
+
+// variables
+var x = 3;
+var y = 5;
+var z = x + y;
+alert(z);
+
+
+// javascript assignment overatop
+var Asl = 39 + "thirty nine";
+document.getElementsByTagName("Asl").innerHTML = asl;
+
+
+var num1 = 3;
+var num2 = 3;
+var num3 = num1 + num2;
+document.innerHTML(num3);
+
+// javascript data types - num , string,  array, object
+
+var age = 29;
+var lastname = "Kumar";
+var cars = ["audi" , "Safari" , "Bulero"]; // array
+var person = {firstname: "Nitin" , lastname: "Kumar" , age: 23, gender:"male"}; // object
