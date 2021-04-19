83da06bf9e30630c57775cc90f68afbfbb0746f5
admin42045 <nitinkumar42045@gmail.com>
2021-04-19 18:30:27 +0530

    edit file again

--- c06/es6.js
+++ c06/es6.js
@@ -19,10 +19,38 @@ let myName = 'nitin';
 myName = 'kumar';
 console.log(myName)
 
+
+// desctructuring in es6
+
+// Array desctructuring
+//const myBioData = ['Nitin', 'Kumar', 23];
+
+
+//let myFName = myBioData[0];
+//let myLName =myBioData[1];
+//let myAge = myBioData[2];
+//console.log(`My name is: ${myFName} ${myLName}  ${myAge}`);
+
+let [myFName,myLName, myAge] = myBioData;
+console.log(myFName);
+console.log(myLName);
+console.log(myAge);
+
+// object desctruting
+const myBioData = {
+  myFName: 'Nitin',
+  myLName: 'Kumar',
+  myAge: '22'
+}
+
+let fname = myBioData.myFName;
+let lname =myBioData.myLName;
+let age=myBioData.myAge;
+
+console.log(fname,lname, age);
+
+
 */
-
-
-
 
 
 
