f00a2389b9a5e03e442d8d2d24facdb291ed716c
admin42045 <nitinkumar42045@gmail.com>
2021-04-19 17:52:43 +0530

    Initial commit

---
+++ c06/es6.js
@@ -0,0 +1,29 @@
+/*
+// ECMA Script and ES6
+//let, const, var
+
+// var -> Function Scope
+// let and const -> Block scope
+
+
+
+// var and let
+// ovrride variable without any error
+
+var myName = "n";
+myName = "k";
+console.log(myName);
+
+
+let myName = 'nitin';
+myName = 'kumar';
+console.log(myName)
+
+*/
+
+
+
+
+
+
+
